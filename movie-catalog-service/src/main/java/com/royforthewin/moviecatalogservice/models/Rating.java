f4da8b8f26157bac19eaddfc9700cc0134ef4f3f
Roy <roynni.zambrano@tata.com.uy>
2019-10-22 01:02:17 -0300

    add rest template and hardcode call of rest services

---
+++ movie-catalog-service/src/main/java/com/royforthewin/moviecatalogservice/models/Rating.java
@@ -0,0 +1,18 @@
+package com.royforthewin.moviecatalogservice.models;
+
+import lombok.Data;
+
+@Data
+public class Rating {
+
+    private String movieId;
+    private int rating;
+
+    public Rating(String movieId, int rating) {
+        this.movieId = movieId;
+        this.rating = rating;
+    }
+
+    public Rating() {
+    }
+}
---
+++ ratings-data-service/src/main/java/com/royforthewin/ratingsdataservice/resources/RatingsResource.java
@@ -0,0 +1,18 @@
+package com.royforthewin.ratingsdataservice.resources;
+
+import com.royforthewin.ratingsdataservice.models.Rating;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsResource {
+
+
+    @RequestMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") String movieId){
+        return new Rating(movieId, 4);
+    }
+
+}
