c602a1b1abcc3d86c51aee3863fc855aabe9904e
Roy <roynni.zambrano@tata.com.uy>
2019-10-22 21:55:21 -0300

    add autowired rest template

--- movie-catalog-service/src/main/java/com/royforthewin/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/royforthewin/moviecatalogservice/resources/MovieCatalogResource.java
@@ -3,6 +3,7 @@ package com.royforthewin.moviecatalogservice.resources;
 import com.royforthewin.moviecatalogservice.models.CatalogItem;
 import com.royforthewin.moviecatalogservice.models.Movie;
 import com.royforthewin.moviecatalogservice.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,10 +18,11 @@ import java.util.stream.Collectors;
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+    @Autowired
+    RestTemplate restTemplate;
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-        RestTemplate restTemplate = new RestTemplate();
-        // Movie movie restTemplate.getForObject("http://localhost:8083/ratingsdata/1",Movie.class);
 
         List<Rating> ratings = Arrays.asList(
                 new Rating("1234", 4),
