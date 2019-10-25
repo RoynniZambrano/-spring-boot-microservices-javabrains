61f27cee40e0ff95e805a5c22b6aa2a8723592f8
Roy <roynny51@gmail.com>
2019-10-25 01:34:38 -0300

    created eureka client and service discovery

--- movie-catalog-service/src/main/java/com/royforthewin/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/royforthewin/moviecatalogservice/resources/MovieCatalogResource.java
@@ -5,6 +5,7 @@ import com.royforthewin.moviecatalogservice.models.Movie;
 import com.royforthewin.moviecatalogservice.models.Rating;
 import com.royforthewin.moviecatalogservice.models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,14 +25,17 @@ public class MovieCatalogResource {
     RestTemplate restTemplate;
 
     @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @Autowired
     WebClient.Builder webClientBuilder;
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
-        UserRating ratings  = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId, UserRating.class);
+        UserRating ratings = restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/" + userId, UserRating.class);
         return ratings.getUserRating().stream().map(rating -> {
-            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+            Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
             return new CatalogItem(movie.getName(), movie.getName(), rating.getRating());
         }).collect(Collectors.toList());
 
