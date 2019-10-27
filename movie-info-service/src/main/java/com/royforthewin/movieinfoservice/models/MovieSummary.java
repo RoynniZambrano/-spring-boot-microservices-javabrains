7935e2c42723fd29c8a6e4dbb9d1fad544bb49c2
Roy <roynny51@gmail.com>
2019-10-27 19:49:44 -0300

    add call to a external api themoviedb to movie services info

---
+++ movie-info-service/src/main/java/com/royforthewin/movieinfoservice/models/MovieSummary.java
@@ -0,0 +1,12 @@
+package com.royforthewin.movieinfoservice.models;
+
+import lombok.Data;
+
+@Data
+public class MovieSummary {
+
+    private String id;
+    private String title;
+    private String overview;
+
+}
